e713242ee3d2ccd9849a7f77d3794e1ae2f5b346
sofyankirat <139446044+sofyankirat@users.noreply.github.com>
2025-01-10 17:22:42 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,1 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import TopReviews from './TopReviews';
 
-function Home() {
-  return (
-    <>
-      <Hero />
-      <TopReviews/>
-      
-    </>
-  );
-}
-
-export default Home
